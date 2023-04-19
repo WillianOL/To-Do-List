d89f65311a462cc177757c7291e8eedf4a88b10d
Will Oliveira <williancontato67@gmail.com>
2023-04-19 09:26:50 -0300

    Atualizações

--- assets/index.js
+++ assets/index.js
@@ -6,10 +6,19 @@ const taskDescription = document.querySelector('#taskDescription');
 function dataVelidation() {
     const tittleValue = taskTittle.value
     const descriptionValue = taskDescription.value
+    const erroMensage = document.querySelector(".erroMensageTask")
 
     if(tittleValue === ''){
+        erroMensage.classList.add('showMensage')        
+    } else{
+        erroMensage.classList.remove('showMensage')
         
+        addTask();
     }
 }
 
 addTaskButton.addEventListener("click", dataVelidation)
+
+function addTask(){
+    
+}
