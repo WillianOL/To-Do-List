4de6895fc7d10534b54daa9414fc6785d6a48555
Will Oliveira <williancontato67@gmail.com>
2023-04-19 20:11:05 -0300

    Modificações

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 const addTaskButton = document.querySelector(".btnAddTask");
 const taskTittle = document.querySelector('#taskTittle');
 const taskDescription = document.querySelector('#taskDescription');
+const conteinerTasks = document.querySelector('.taskConteiner')
 
 
 function dataVelidation() {
@@ -22,6 +23,13 @@ function addTask(){
     const tittleTask = taskTittle.value
     const descriptionTask = taskDescription.value
 
-    
-    console.log(tittleTask, descriptionTask);
+    conteinerTasks.innerHTML += `
+    <div class="task">
+        <div>
+            <h1 class="taskTittle">${tittleTask}</h1>
+            <p class="taskTittle">${descriptionTask}</p>
+        </div>
+        <button class="removeTask" title="Remover tarefa">-</button>
+    </div>`
+
 }
