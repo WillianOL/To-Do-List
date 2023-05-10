f33768ce1ddcd115bf405b9198768122ee50e34a
Will Oliveira <williancontato67@gmail.com>
2023-05-10 19:35:03 -0300

    atualizações

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 const buttonAddTask = document.querySelector("[data-botaoTask='adicionar']");
 const containsTaskErro = document.querySelector("[data-erroMensage='taskIsIncluded']");
 const valueErroMensage = document.querySelector("[data-erroMensage='valueErro']");
+const taskContainer = document.querySelector(".taskArea")
 
 const tasksBox = [];
 
@@ -29,10 +30,20 @@ function adicionarTask() {
     const taskTittle = document.querySelector("#taskTittle");
     const taskDescription = document.querySelector("#taskDescription");
 
-}
+    const taskCreate = `
+        <div class="taskConteiner">
+            <div class="task">
+                <h1>${taskTittle.value}</h1>
+                <p>${taskDescription.value}</p>
+            </div>
+                <button class="removeTask" data-botaoTask="remover" title="Remover anotação">-</button>
+            </div>
+    `
+    taskContainer.innerHTML += taskCreate
+}   
 
 function removerTask() {
-
+    console.log("FOI");
 }
 
 buttonAddTask.addEventListener("click", taskVerification)
