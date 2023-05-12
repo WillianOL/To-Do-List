51b19bb74d108bea9584e6eebf44e60a7bed3293
Will Oliveira <williancontato67@gmail.com>
2023-05-11 21:37:48 -0300

    atualizações

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,19 @@
 const buttonAddTask = document.querySelector("[data-buttons='addTask']");
 const titleTask = document.querySelector("#taskTittle");
+const titleErrorMessage = document.querySelector("[data-erroMensage='erroTitle']");
+const IncludesErrorMensage = document.querySelector("[data-erroMensage='erroTaskIncludes']")
+
 const taskTitles = [];
 
 function inputValueVerification(event) {
     event.preventDefault();
-    const removesValueErrorClass = ;
-
     const containsTask = taskTitles.includes(titleTask.value);
-    if (containsTask) {
+    const removesValueErrorClass = titleErrorMessage.classList.remove("showErroMensage");
+
+    if (titleTask.value == "") {
+        titleErrorMessage.classList.add("showErroMensage")
+    } else if(containsTask) {
+
     }
 }
 
