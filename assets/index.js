15ab40b5706c08a3f226122ce0d497bc1fee0360
Will Oliveira <williancontato67@gmail.com>
2023-05-13 20:41:53 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 const botaoAdicionarTarefa = document.querySelector("[data-buttons='addTask']");
 const tituloTarefa = document.querySelector("#taskTittle");
+const descricaoTarefa = document.querySelector("#taskDescription")
 const mensagemErroTitulo = document.querySelector("[data-erroMensage='erroTitle']");
 const mensagemTemTarefa = document.querySelector("[data-erroMensage='erroTaskIncludes']")
 
@@ -19,7 +20,25 @@ function verificarValorDoInput(event) {
         mensagemTemTarefa.classList.add("showErrorMensage");
     } else {
         arrayTarefas.push(tituloTarefa.value);
+        adicionarTarefa();
+        tituloTarefa.value = ""
     }
 }
-
 botaoAdicionarTarefa.addEventListener("click", verificarValorDoInput);
+
+function adicionarTarefa() {
+    const tituloValor = tituloTarefa.value;
+    const descricaoValor = descricaoTarefa.value;
+    const tarefaConteiner = document.querySelector(".taskArea ul");
+
+    tarefaConteiner.innerHTML += `
+    <li>
+        <div class="task">
+            <h1>${tituloValor}</h1>
+            <p>${descricaoValor}</p>
+        </div>
+        <button data-botaoTask="remover" class="remover" title="Remover anotação">-</button>
+    </li>
+    `
+
+}
